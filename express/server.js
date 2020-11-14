8e267a8a2e05d2d67cfe0dd0f40c9152ea214c8a
adamgoth <adamgoth@gmail.com>
2020-11-14 12:10:46 -0700

    Initial commit

---
+++ express/server.js
@@ -0,0 +1,30 @@
+'use strict'
+const express = require('express')
+const path = require('path')
+const serverless = require('serverless-http')
+const app = express()
+const bodyParser = require('body-parser')
+const router = express.Router()
+
+app.use(bodyParser.json())
+app.use('/.netlify/functions/server', router) // path must route to lambda
+app.use('/', router)
+
+router.get('/', (req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html' })
+  res.write('<h1>Up and running</h1>')
+  res.end()
+})
+
+router.post('/doSomething', async (req, res) => {
+  try {
+    // maybe do some database interaction or third-party API call here!
+    res.status(200).send({ data: 'success' })
+  } catch (err) {
+    console.log(err)
+    res.status(400).send({ error: 'bad request' })
+  }
+})
+
+module.exports = app
+module.exports.handler = serverless(app)
